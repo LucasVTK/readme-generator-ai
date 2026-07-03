77540208e77e7f60defe28f1b0460103174a8b95
Lucas Nog <lucazim.vtk@gmail.com>
2026-07-02 23:20:32 -0300

    fix: ajuste de posicionamento de cors para deploy

--- Server/src/server.js
+++ Server/src/server.js
@@ -2,17 +2,19 @@ import express from "express"
 import ReadmeRoute from "./Route/ReadmeRoute.js"
 import MiddlewareCros from "./middleware/MiddlewareCros.js"
 
-
 //configuracoes basicas de servidor
 const app = express()
 const PORT = process.env.PORT
 const HOST = process.env.HOST  
 
+//CORS AUTORIZACAO 
+app.use(MiddlewareCros.cors)
+
+
 app.use(express.json())
 app.use(ReadmeRoute)
 
-//CORS AUTORIZACAO
-app.use(MiddlewareCros.cors)
+
 
 app.get("/",(req,res) =>{
     res.status(200).json({
